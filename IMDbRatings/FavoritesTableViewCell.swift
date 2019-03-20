23a25f30fab4b336767aaf34afaac464598c8696
Ekaterina <zireael6@gmail.com>
2019-03-20 14:35:16 +0300

    избранные фильмы теперь отображаются в таблице избранных

---
+++ IMDbRatings/FavoritesTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesTableViewCell.swift
+//  IMDbRatings
+//
+//  Created by Ekaterina Kozlova on 20.03.2019.
+//  Copyright © 2019 Ekaterina Kozlova. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- IMDbRatings/FavoritesVC.swift
+++ IMDbRatings/FavoritesVC.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+import Foundation
+import RealmSwift
 
-class FavoritesVC: UIViewController
+class FavoritesVC: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-
+    var favorites: [Film] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteCell", for: indexPath) as! FavoritesTableViewCell
+        if favorites.count != 0
+        {
+            cell.descriptionLabel.text = favorites[indexPath.row].overview
+            if let url = URL(string: favorites[indexPath.row].poster)
+            {
+                cell.posterImageView.downloadedFrom(url: url)
+            }
+            cell.titleLabel.text = favorites[indexPath.row].title
+            cell.ratingLabel.text = "Рейтинг: " + String(favorites[indexPath.row].rate)
+        }
+        return cell
+    }
+    
+    @IBOutlet weak var favoritesTableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        let realm = try! Realm()
+        favorites = Array(realm.objects(Film.self))
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        favoritesTableView.delegate = self
+        favoritesTableView.dataSource = self
     }
-
 }
